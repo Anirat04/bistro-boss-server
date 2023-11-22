ad7c2e0a1699cba500482dc664da66dc471438a4
Tanbir Haque <tanbirhaque53@gmail.com>
2023-11-23 04:56:06 +0600

    Cart collection get and post method complete

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         const menuCollection = client.db('Bistro-Boss_DB').collection('menu');
         const reviewsCollection = client.db('Bistro-Boss_DB').collection('review');
+        const cartsCollection = client.db('Bistro-Boss_DB').collection('carts');
 
         // getting all menu
         app.get('/menu', async(req, res) => {
@@ -36,6 +37,19 @@ async function run() {
         // getting all reviews
         app.get('/reviews', async(req, res) =>{
             const result = await reviewsCollection.find().toArray();
+            res.send(result)
+        })
+        // post all carts to the carts collection
+        app.post('/carts', async(req, res) => {
+            const cartItem = req.body
+            const result = await cartsCollection.insertOne(cartItem)
+            res.send(result)
+        })
+        // create API to getting all carts
+        app.get('/carts', async(req, res) => {
+            const email = req.query.email
+            const query = {userEmail: email}
+            const result =  await cartsCollection.find(query).toArray();
             res.send(result)
         })
 
